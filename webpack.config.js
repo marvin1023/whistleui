3f1dfe8277b82dab0515e3ccafca623a1ae1ceca
avwo <avwu@qq.com>
2015-08-16 22:39:19 +0800

    add jsxhint

--- webpack.config.js
+++ webpack.config.js
@@ -10,5 +10,20 @@ module.exports = {
     path: __dirname + '/dist/',
     filename: '[name].js'
   },
+  module: {
+	    preLoaders: [{
+	      test: /\.js$/,
+	      exclude: /node_modules/,
+	      loader: 'jsxhint'
+	    }],
+	    loaders: [{
+	      test: /\.js$/,
+	      exclude: /node_modules/,
+	      loader: 'react-hot!jsx-loader?harmony'
+	    }, {
+	      test: /\.css$/,
+	      loader: 'style-loader!css-loader'
+	    }]
+	  },
   plugins: [new webpack.optimize.CommonsChunkPlugin('common.js')]
 };
