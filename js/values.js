31aba42faf6f801d40e4791f0179915256274c38
avwo <avwu@qq.com>
2015-08-17 21:00:50 +0800

    values

--- js/values.js
+++ js/values.js
@@ -1,2 +1,12 @@
-require('./css');
-var react = require('react');
+require('./base-css.js');
+var React = require('react');
+var List = require('./list');
+var Menu = require('./menu');
+
+React.render(
+		<div className="main box-orient-vertical">
+			<div className="w-rules-con"></div>
+			<div className="w-network-con"></div>
+			<Menu />
+			<List />
+		</div>, document.body);
