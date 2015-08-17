7e687e86a3df142d89cbe299cf3735dd8bb26991
avwo <avwu@qq.com>
2015-08-17 20:23:33 +0800

    base.css

--- dist/test.js
+++ dist/test.js
@@ -20852,15 +20852,30 @@
 	var Divider = React.createClass({displayName: "Divider",
 		componentDidMount: function() {
 			var divider = this.refs.divider.getDOMNode();
-			var left = 1;
-			var right = 1;
-			if (/^(\d+):(\d+)$/.test(this.props.rate)) {
-				left = parseInt(RegExp.$1, 10) || 1;
-				right = parseInt(RegExp.$2, 10) || 1;
-			}
+			var leftWidth = parseInt(this.props.leftWidth, 10);
+			var rightWidth;
 			var width = this.props.vertical ? divider.offsetHeight : divider.offsetWidth;
+			if (leftWidth > 0) {
+				rightWidth = width - leftWidth;
+			}
+			
+			if (!(rightWidth > 0)) {
+				rightWidth = parseInt(this.props.rightWidth, 10);
+			}
+			
+			if (!(rightWidth > 0)) {
+				var left = 1;
+				var right = 1;
+				if (/^(\d+):(\d+)$/.test(this.props.rate)) {
+					left = parseInt(RegExp.$1, 10) || 1;
+					right = parseInt(RegExp.$2, 10) || 1;
+				}
+				
+				rightWidth = width * right / (left + right);
+			}
+			
 			$(divider).find('.w-divider-right')[this.props.vertical ?
-					'height' : 'width'](width * right / (left + right));
+					'height' : 'width'](rightWidth);
 		},
 		render: function() {
 			var vertical = this.props.vertical;
