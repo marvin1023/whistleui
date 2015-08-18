879e358bac8b2f0ea52202070dff6ad3211863e8
avwo <avwu@qq.com>
2015-08-18 23:21:52 +0800

    list

--- dist/test.js
+++ dist/test.js
@@ -30341,12 +30341,8 @@
 			this._data = {};
 			this._list = [];
 		},
-		exists: function(name) {
-			
-			return typeof name == 'string' ? this._data[name] : false;
-		},
 		add: function(name, value) {
-			if (this.exists(name)) {
+			if (this.getItem(name)) {
 				return false;
 			}
 			var list = this._list;
@@ -30368,54 +30364,114 @@
 			return true;
 		},
 		remove: function(name) {
-			
+			delete this._data[name];
+			var index = this._list.indexOf(name);
+			if (index != -1) {
+				this._list.splice(index, 1);
+				this.forceUpdate();
+			}
 		},
 		rename: function(name, newName) {
-			
+			var item = this.getItem(name);
+			if (item) {
+				this._list[this._list.indexOf(name)] = item.name = newName;
+				this.forceUpdate();
+			}
 		},
 		select: function(name) {
-			
+			var item = this.getItem(name);
+			if (item) {
+				var data = this._data;
+				Object.keys(data).forEach(function(name) {
+					data[name].selected = false;
+				});
+				item.selected = true;
+				this.forceUpdate();
+			}
 		},
 		unselect: function(name) {
-			
+			if (!arguments.length) {
+				Object.keys(data).forEach(function(name) {
+					data[name].selected = false;
+				});
+				this.forceUpdate();
+			}else if (name = this.getItem(name)) {
+				name.selected = false;
+				this.forceUpdate();
+			}
 		},
 		enable: function(name) {
-			
+			if (name == getItem(name)) {
+				name.active = true;
+				this.forceUpdate();
+			}
 		},
 		disable: function(name) {
-			
+			if (!argumetns.length) {
+				Object.keys(data).forEach(function(name) {
+					data[name].active = false;
+				});
+				this.forceUpdate();
+			} else if (name == getItem(name)) {
+				name.active = false;
+				this.forceUpdate();
+			}
 		},
-		getCurrentItem: function() {
-			
+		getSelectedItem: function() {
+			var selectedItem;
+			for (var i in this._data) {
+				var item = this._data[i];
+				if (!selectedItem && item.selected) {
+					selectedItem = item;
+				} else {
+					item.selected = false;
+				}
+			}
+			return selectedItem;
 		},
 		getItem: function(name) {
-			
+			return this._data[name];
 		},
 		componentDidMount: function() {
 			
 		},
-		onMouseEnter: function(e) {
+		_onMouseEnter: function(e) {
 			$(e.target).closest('a').addClass('w-hover');
 		},
-		onMouseLeave: function(e) {
+		_onMouseLeave: function(e) {
 			$(e.target).closest('a').removeClass('w-hover');
 		},
-		onClick: function(e) {
+		_onClick: function(e) {
 			var elem = $(e.target).closest('a');
 			var item = this._getItemByKey(elem.attr('data-key'));
-			var data = this._data;
-			Object.keys(data).forEach(function(name) {
-				data[name].selected = false;
-			});
-			item.selected = true;
-			this.forceUpdate();
+			if (!item || typeof this.props.onSelect == 'function' && 
+					this.props.onSelect({target: elem, data: item}) === false) {
+				return;
+			}
+			this.select(item.name);
 		},
-		onDoubleClick: function(e) {
+		_onDoubleClick: function(e) {
 			var elem = $(e.target).closest('a');
 			var item = this._getItemByKey(elem.attr('data-key'));
-			item.active = true;
-			this.forceUpdate();
-			//this.select(item && item.name);
+			var e = {
+					target: elem,
+					data: item
+			};
+			item.active ? this.__onDisable(e) : this._onEnable(e);
+		},
+		_onEnable: function(e) {
+			if (typeof this.props.onEnable == 'function' && 
+					this.props.onEnable(e) !== false) {
+				item.active = true;
+				this.forceUpdate();
+			}
+		},
+		_onDisable: function(e) {
+			if (typeof this.props.onDisable == 'function' && 
+					this.props.onEnable(e) !== false) {
+				item.active = false;
+				this.forceUpdate();
+			}
 		},
 		_getItemByKey: function(key) {
 			for (var i in this._data) {
@@ -30430,19 +30486,11 @@
 			var modal = self.props.modal || {};
 			var list = self._list = modal.list || [];
 			var data = self._data = modal.data || {};
-			var selectedItem;
 			list.forEach(function(name) {
 				var item = data[name];
 				if (item) {
 					item.key = item.key || getKey();
 					item.name = name;
-					if (item.selected) {
-						if (selectedItem) {
-							item.selected = false;
-						} else {
-							selectedItem = item;
-						}
-					}
 					return;
 				}
 				data[name] = {
@@ -30452,13 +30500,11 @@
 				};
 			});
 			
+			var selectedItem = self.getSelectedItem();
 			if (!selectedItem && list[0]) {
 				selectedItem = data[list[0]];
 				selectedItem.selected = true;
 			}
-			
-			self._onItemClick = self.props.onItemClick;
-			self._onItemDoubleClick = self.props.onDoubleClick;
 			
 			return (
 					React.createElement(Divider, {leftWidth: "200"}, 
@@ -30467,10 +30513,10 @@
 								list.map(function(name) {
 									var item = data[name];
 									return React.createElement("a", {key: item.key, "data-key": item.key, 
-												onMouseEnter: self.onMouseEnter, 
-												onMouseLeave: self.onMouseLeave, 
-												onClick: self.onClick, 
-												onDoubleClick: self.onDoubleClick, 
+												onMouseEnter: self._onMouseEnter, 
+												onMouseLeave: self._onMouseLeave, 
+												onClick: self._onClick, 
+												onDoubleClick: self._onDoubleClick, 
 												className: (item.selected ? 'w-selected' : '') + (item.active ? ' w-active' : ''), 
 												href: "javascript:;"}, name, React.createElement("span", {className: "glyphicon glyphicon-ok"}));
 								})
