df27afb22bd1b958b7dea67ecc86b83814cb8da5
avwo <avwu@qq.com>
2015-08-19 15:16:21 +0800

    add btn-group

---
+++ js/btn-group.js
@@ -0,0 +1,12 @@
+require('./base-css.js');
+var React = require('react');
+
+React.createClass({
+	render: function() {
+		var group = this.props.modal;
+		
+		return (
+			<div></div>
+		);
+	}
+});
