56a8c6ec64575daa2598639e821f64332bdc8bcf
avwo <avwu@qq.com>
2015-08-22 13:52:20 +0800

    add cgi

---
+++ js/cgi.js
@@ -0,0 +1,59 @@
+var $ = require('jquery');
+
+function createCgi(url, settings) {
+	var self = this;
+	if (typeof url == 'string') {
+		url = {url: url};
+	}
+	settings = $.extend({}, settings, url);
+	var queue = [];
+	var jqXhr;
+	
+	function cgiFn(data, callback, options) {
+		if (typeof data == 'function') {
+			options = callback;
+			callback = data;
+			data = null;
+		}
+		
+		var options = $.extend({}, settings, optons);
+		if (jqXhr) {
+			var mode = options.mode;
+			if (mode == 'ignore') {
+				return;
+			}
+			if (mode == 'cancel') {
+				jqXhr.abort();
+			} else if (mode == 'chain') {
+				queue.push([data, callback, options]);
+			}
+		}
+		
+		var execCallback = function(data, xhr) {
+			jqXhr = null;
+			callback && callback.call(this, data, xhr);
+			var args = queue.shift();
+			args && cgiFn.apply(self, args);
+		};
+		options.success = function(data, statusText, xhr) {
+			execCallback.call(this, data, xhr);
+		};
+		options.error = function(xhr) {
+			execCallback.call(this, false, xhr)
+		};
+		
+		return (jqXhr = $.ajax(options));
+	}
+	
+	return cgiFn;
+}
+
+function create(obj, settings) {
+	var cgi = {};
+	Object.keys(obj).forEach(function(name) {
+		cgi[name] = createCgi(obj[name], settings);
+	});
+	return cgi;
+}
+
+module.exports = create;
