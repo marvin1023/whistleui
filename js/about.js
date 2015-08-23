ab90873ab0bc6bc396b38c673eee5ea7c0f6475a
avwo <avwu@qq.com>
2015-08-24 00:11:20 +0800

    about

--- js/about.js
+++ js/about.js
@@ -14,7 +14,7 @@ function createDialog(version) {
 				      '<div class="modal-body">' + 
 				      '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
 				        '<img alt="logo" src="/img/whistle.png">' + 
-			          '<span" class="w-about-dialog-ctn">Whistle for Web Developers.<br><br>' +
+			          '<span" class="w-about-dialog-ctn"><span class="w-about-dialog-title">Whistle for Web Developers.</span>' +
 					  'Version: <span id="aboutVersion">' + version + '</span><br>' + 
 					  'Visit <a id="aboutUrl" href="http://www.whistlejs.com#v=' + version + '" target="_blank">http://www.whistlejs.com</a></span>' +
 				      '</div>' + 
