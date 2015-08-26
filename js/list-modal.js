62df18d19e179543b6d46b25b4ee79c32496f9ab
avwo <avwu@qq.com>
2015-08-26 17:26:21 +0800

    add network modal

--- js/list-modal.js
+++ js/list-modal.js
@@ -1,4 +1,3 @@
-var $ = require('jquery');
 var util = require('./util');
 
 function ListModal(list, data) {
---
+++ js/network-modal.js
@@ -0,0 +1,6 @@
+function NetworkModal(list) {
+	
+}
+
+var proto = NetworkModal.prototype;
+
