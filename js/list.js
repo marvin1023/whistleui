48a7d21c26d0c9a6f551150960890e8dc6680f0e
avwo <avwu@qq.com>
2015-08-28 16:25:54 +0800

    list

--- js/list.js
+++ js/list.js
@@ -15,102 +15,59 @@ function getSuffix(name) {
 }
 
 var List = React.createClass({
-	componentWillMount: function() {
-		this._data = {};
-		this._list = [];
-	},
-	add: function(name, value) {
-		var modal = this.props.modal;
-		if (modal.add(name, value)) {
-			modal.setActive(name);
-			this.forceUpdate();
-			return true;
-		}
-		return false;
-	},
-	remove: function(name) {
-		var modal = this.props.modal;
-		var index = modal.getIndex(name);
-		if (modal.remove(name) && (name = modal.list[index] 
-					|| modal.list[index - 1])) {
-			modal.setActive(name);
-			this.forceUpdate();
-		}
-	},
-	getActiveItem: function() {
-		
-		return this.props.modal.getActive();
-	},
-
-	getItem: function(name) {
-		return this._data[name];
-	},
 	componentDidMount: function() {
 		var self = this;
+		var visible = !self.props.hide;
 		var list = $(self.refs.list.getDOMNode());
 		$(window).keydown(function(e) {
-			if (isSaveFile(e)) {
-				list.find('.w-changed').each(trigger);
-				return false;
-			}
-		});
-		
-		var editor = $(this.refs.editor.getDOMNode()).keydown(function(e) {
-			if (isSaveFile(e)) {
-				var selectedElem = list.find('.w-active');
-				if (selectedElem.hasClass('w-changed') || !selectedElem.hasClass('w-selected')) {
-					selectedElem.each(trigger);
-				}
+			if (visible && (e.ctrlKey || e.metaKey) && e.keyCode == 83) {
+				list.find('.w-changed,.w-active').each(trigger);
 				return false;
 			}
 		});
 		
 		function trigger() {
-			self._onDoubleClick({target: this});
-		}
-		
-		function isSaveFile(e) {
-			return editor.is(':visible') && (e.ctrlKey || e.metaKey) && e.keyCode == 83;
+			self.onDoubleClick({target: this});
 		}
 	},
-	_onMouseEnter: function(e) {
-		this._getItemByKey($(e.target).closest('a').attr('data-key')).hover = true;
+	onMouseEnter: function(e) {
+		this.getItemByKey($(e.target).closest('a').attr('data-key')).hover = true;
 		this.forceUpdate();
 	},
-	_onMouseLeave: function(e) {
-		this._getItemByKey($(e.target).closest('a').attr('data-key')).hover = false;
+	onMouseLeave: function(e) {
+		this.getItemByKey($(e.target).closest('a').attr('data-key')).hover = false;
 		this.forceUpdate();
 	},
-	_onClick: function(e) {
+	onClick: function(e) {
 		var elem = $(e.target).closest('a');
-		var item = this._getItemByKey(elem.attr('data-key'));
+		var item = this.getItemByKey(elem.attr('data-key'));
 		if (!item || (typeof this.props.onActive == 'function' && 
 				this.props.onActive(item) === false)) {
 			return;
 		}
 		this.props.modal.setActive(item.name);
 	},
-	_onDoubleClick: function(e) {
+	onDoubleClick: function(e) {
 		var target = $(e.target);
 		var elem = target.closest('a');
-		var item = this._getItemByKey(elem.attr('data-key'));
+		var item = this.getItemByKey(elem.attr('data-key'));
 		var okIcon = target.hasClass('glyphicon-ok');
-		item.selected && !item.changed || okIcon ? this._onDisable(item) : this._onEnable(item);
+		item.selected && !item.changed || okIcon ? this.onDisable(item) : this.onEnable(item);
 		okIcon && e.stopPropagation();
 	},
-	_onEnable: function(data) {
+	onEnable: function(data) {
 		if (typeof this.props.onEnable != 'function' || 
 				this.props.onEnable.call(this, data) !== false) {
 			this.enable(data.name);
 		}
 	},
-	_onDisable: function(data) {
+	onDisable: function(data) {
 		if (typeof this.props.onDisable != 'function' || 
 				this.props.onDisable.call(this, data) !== false) {
 			this.disable(data.name);
 		}
 	},
-	_onChange: function(e) {
+	onChange: function(e) {
 		var item = this.props.modal.getActive();
 		if (!item) {
 			return;
@@ -123,14 +80,14 @@ var List = React.createClass({
 			this.forceUpdate();
 		}
 	},
-	_getItemByKey: function(key) {
+	getItemByKey: function(key) {
 		return this.props.modal.getByKey(key);
 	},
 	render: function() {
 		var self = this;
 		var modal = self.props.modal;
-		var list = self._list = modal.list;
-		var data = self._data = modal.data;
+		var list = modal.list;
+		var data = modal.data;
 		var activeItem = modal.getActive();
 		
 		return (
@@ -140,19 +97,19 @@ var List = React.createClass({
 							list.map(function(name) {
 								var item = data[name];
 								return <a key={item.key} data-key={item.key}
-											onMouseEnter={self._onMouseEnter}
-											onMouseLeave={self._onMouseLeave}
-											onClick={self._onClick} 
-											onDoubleClick={self._onDoubleClick} 
+											onMouseEnter={self.onMouseEnter}
+											onMouseLeave={self.onMouseLeave}
+											onClick={self.onClick} 
+											onDoubleClick={self.onDoubleClick} 
 											className={(item.hover ? 'w-hover' : '') 
 											+ (item.active ? ' w-active' : '') 
 											+ (item.changed ? ' w-changed' : '')
 											+ (item.selected ? ' w-selected' : '')} 
-											href="javascript:;">{name}<span onClick={self._onDoubleClick} className="glyphicon glyphicon-ok"></span></a>;
+											href="javascript:;">{name}<span onClick={self.onDoubleClick} className="glyphicon glyphicon-ok"></span></a>;
 							})
 						}
 					</div>
-					<Editor {...self.props} ref="editor" onChange={self._onChange} readOnly={!activeItem} value={activeItem && activeItem.value} 
+					<Editor {...self.props} onChange={self.onChange} readOnly={!activeItem} value={activeItem ? activeItem.value : ''} 
 					mode={self.props.name == 'rules' ? 'rules' : getSuffix(activeItem && activeItem.name)} />
 				</Divider>
 		);
