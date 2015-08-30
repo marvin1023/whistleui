3951a1dcf9ba61f3ff7c39e69984955b71c867c3
avwo <avwu@qq.com>
2015-08-30 15:32:16 +0800

    height: 0

--- js/list.js
+++ js/list.js
@@ -82,10 +82,10 @@ var List = React.createClass({
 		var list = modal.list;
 		var data = modal.data;
 		var activeItem = modal.getActive();
-		
+		//不设置height为0，滚动会有问题
 		return (
 				<Divider hide={this.props.hide} leftWidth="200">
-					<div ref="list" className={'fill orient-vertical-box w-list-data ' + (this.props.className || '')}>
+					<div style={{height: 0}} ref="list" className={'fill orient-vertical-box w-list-data ' + (this.props.className || '')}>
 						{
 							list.map(function(name) {
 								var item = data[name];
