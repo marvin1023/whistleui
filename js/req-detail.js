44b8448fe766394418bb2745091933808aa1e690
avwo <avwu@qq.com>
2015-09-07 02:40:15 +0800

    disable update if hide

--- js/req-detail.js
+++ js/req-detail.js
@@ -9,31 +9,38 @@ var BtnGroup = require('./btn-group');
 var BTNS = [{name: 'Headers', active: true}, {name: 'TextView'}, {name: 'Cookies'}, {name: 'WebForms'}, {name: 'Raw'}];
 
 var ReqDetail = React.createClass({
+	getInitialState: function() {
+		return {
+			initedHeaders: false,
+			initedTextView: false,
+			initedCookies: false,
+			initedWebForms: false,
+			initedRaw: false
+		};
+	},
 	shouldComponentUpdate: function(nextProps) {
 		var hide = util.getBoolean(this.props.hide);
 		return hide != util.getBoolean(nextProps.hide) || !hide;
 	},
 	onClickBtn: function(btn) {
-		this.setState({btn: btn});
+		var state = {btn: btn};
+		state['inited' + btn.name] = true;
+		this.setState(state);
 	},
 	render: function() {
+		var btn = this.state.btn;
+		var name = btn && btn.name;
 		return (
-			<div className={'fill orient-vertical-box w-detail-request w-detail-show-request-' 
-					+ util.getProperty(this, 'state.btn.name', '').toLowerCase()
-					+ (util.getBoolean(this.props.hide) ? ' hide' : '')}>
+			<div className={'fill orient-vertical-box w-detail-request' + (util.getBoolean(this.props.hide) ? ' hide' : '')}>
 				<BtnGroup onClick={this.onClickBtn} btns={BTNS} />
-				<div className="w-detail-request-headers">
-					<Properties />
-				</div>
-				<textarea onKeyDown={util.preventDefault} readOnly="readonly" className="orient-vertical-box w-detail-request-textview"></textarea>
-				<div className="w-detail-request-cookies">
-					<Properties />
-				</div>
-				<Divider leftWidth={(Math.max(window.innerHeight, 360) - 120) / 2} vertical="true" className="w-detail-request-webforms">
+				{this.state.initedHeaders ? <div className={'w-detail-request-headers' + (name == BTNS[0].name ? '' : ' hide')}><Properties /></div> : ''}
+				{this.state.initedTextView ? <textarea onKeyDown={util.preventDefault} readOnly="readonly" className={'orient-vertical-box w-detail-request-textview' + (name == BTNS[1].name ? '' : ' hide')}></textarea> : ''}
+				{this.state.initedCookies ? <div className={'w-detail-request-cookies' + (name == BTNS[2].name ? '' : ' hide')}><Properties /></div> : ''}
+				{this.state.initedWebForms ? <Divider leftWidth={(Math.max(window.innerHeight, 360) - 120) / 2} vertical="true" className={'w-detail-request-webforms' + (name == BTNS[3].name ? '' : ' hide')}>
 					<Properties />
 					<Properties />
-				</Divider>
-				<textarea onKeyDown={util.preventDefault} readOnly="readonly" className="orient-vertical-box w-detail-request-raw"></textarea>
+				</Divider> : ''}
+				{this.state.initedRaw ? <textarea onKeyDown={util.preventDefault} readOnly="readonly" className={'orient-vertical-box w-detail-request-raw' + (name == BTNS[4].name ? '' : ' hide')}></textarea> : ''}
 			</div>
 		);
 	}
--- js/res-detail.js
+++ js/res-detail.js
@@ -9,29 +9,37 @@ BTNS = [{name: 'Headers', active: true}, {name: 'TextView'}, {name: 'Cookies'}, 
 var COOKIE_HEADERS = ['Name', 'Value', 'Domain', 'Path', 'Http Only', 'Secure'];
 
 var ResDetail = React.createClass({
+	getInitialState: function() {
+		return {
+			initedHeaders: false,
+			initedTextView: false,
+			initedCookies: false,
+			initedJSON: false,
+			initedRaw: false
+		};
+	},
 	shouldComponentUpdate: function(nextProps) {
 		var hide = util.getBoolean(this.props.hide);
 		return hide != util.getBoolean(nextProps.hide) || !hide;
 	},
 	onClickBtn: function(btn) {
-		this.setState({btn: btn});
+		var state = {btn: btn};
+		state['inited' + btn.name] = true;
+		this.setState(state);
 	},
 	render: function() {
-		
+		var btn = this.state.btn;
+		var name = btn && btn.name;
 		return (
 			<div className={'fill orient-vertical-box w-detail-response w-detail-show-response-' 
 				+ util.getProperty(this, 'state.btn.name', '').toLowerCase() 
 				+ (util.getBoolean(this.props.hide) ? ' hide' : '')}>
 				<BtnGroup onClick={this.onClickBtn} btns={BTNS} />
-				<div className="w-detail-response-headers">
-					<Properties />
-				</div>
-				<textarea onKeyDown={util.preventDefault} readOnly="readonly" className="orient-vertical-box w-detail-response-textview"></textarea>
-				<div className="w-detail-response-cookies">
-					<Table head={COOKIE_HEADERS} />
-				</div>
-				<div className="w-detail-response-json"></div>
-				<textarea onKeyDown={util.preventDefault} readOnly="readonly" className="orient-vertical-box w-detail-response-raw"></textarea>
+				{this.state.initedHeaders ? <div className={'w-detail-response-headers' + (name == BTNS[0].name ? '' : ' hide')}><Properties /></div> : ''}
+				{this.state.initedTextView ? <textarea onKeyDown={util.preventDefault} readOnly="readonly" className={'orient-vertical-box w-detail-response-textview' + (name == BTNS[1].name ? '' : ' hide')}></textarea> : ''}
+				{this.state.initedCookies ? <div className={'w-detail-response-cookies' + (name == BTNS[2].name ? '' : ' hide')}><Table head={COOKIE_HEADERS} /></div> : ''}
+				{this.state.initedJSON ? <textarea onKeyDown={util.preventDefault} className={'w-detail-response-json' + (name == BTNS[3].name ? '' : ' hide')}></textarea> : ''}
+				{this.state.initedRaw ? <textarea onKeyDown={util.preventDefault} readOnly="readonly" className={'orient-vertical-box w-detail-response-raw' + (name == BTNS[4].name ? '' : ' hide')}></textarea> : ''}
 			</div>
 		);
 	}
