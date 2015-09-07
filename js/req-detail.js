03fe8a8c3560e243539e3aeed7543840f05e76d4
avwo <avwu@qq.com>
2015-09-08 01:20:05 +0800

    table

--- js/req-detail.js
+++ js/req-detail.js
@@ -56,10 +56,10 @@ var ReqDetail = React.createClass({
 		return (
 			<div className={'fill orient-vertical-box w-detail-content w-detail-request' + (util.getBoolean(this.props.hide) ? ' hide' : '')}>
 				<BtnGroup onClick={this.onClickBtn} btns={BTNS} />
-				{this.state.initedHeaders ? <div className={'w-detail-request-headers' + (name == BTNS[0].name ? '' : ' hide')}><Properties modal={headers} /></div> : ''}
-				{this.state.initedTextView ? <textarea value={body} onKeyDown={util.preventDefault} readOnly="readonly" className={'orient-vertical-box w-detail-request-textview' + (name == BTNS[1].name ? '' : ' hide')}></textarea> : ''}
-				{this.state.initedCookies ? <div className={'w-detail-request-cookies' + (name == BTNS[2].name ? '' : ' hide')}><Properties modal={cookies} /></div> : ''}
-				{this.state.initedWebForms ? <Divider vertical="true" className={'w-detail-request-webforms' + (name == BTNS[3].name ? '' : ' hide')}>
+				{this.state.initedHeaders ? <div className={'fill w-detail-request-headers' + (name == BTNS[0].name ? '' : ' hide')}><Properties modal={headers} /></div> : ''}
+				{this.state.initedTextView ? <textarea value={body} onKeyDown={util.preventDefault} readOnly="readonly" className={'fill w-detail-request-textview' + (name == BTNS[1].name ? '' : ' hide')}></textarea> : ''}
+				{this.state.initedCookies ? <div className={'fill w-detail-request-cookies' + (name == BTNS[2].name ? '' : ' hide')}><Properties modal={cookies} /></div> : ''}
+				{this.state.initedWebForms ? <Divider vertical="true" className={'fill w-detail-request-webforms' + (name == BTNS[3].name ? '' : ' hide')}>
 					<Properties />
 					<Properties />
 				</Divider> : ''}
