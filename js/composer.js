3f509b7c3b97fd9f628cee5bd70e3c37f8524333
avwo <avwu@qq.com>
2015-09-08 14:56:01 +0800

    add composer

--- js/composer.js
+++ js/composer.js
@@ -8,25 +8,38 @@ var Divider = require('./divider');
 var Composer = React.createClass({
 	componentDidMount: function() {
 		var self = this;
-		var activeItem = self.props.modal;
+		self.update(self.props.modal);
 		events.on('composer', function() {
+			var activeItem = self.props.modal;
 			activeItem && self.setState({
 				data: activeItem
+			}, function() {
+				self.update(activeItem);
 			});
 		});
+	},
+	update: function(item) {
+		if (!item) {
+			return;
+		}
+		var refs = this.refs;
+		var req = item.req;
+		refs.url.getDOMNode().value = item.url;
+		refs.method.getDOMNode().value = req.method;
+		refs.headers.getDOMNode().value = util.objectToString(req.headers);
+		refs.body.getDOMNode().value = req.body || '';
 	},
 	shouldComponentUpdate: function(nextProps) {
 		var hide = util.getBoolean(this.props.hide);
 		return hide != util.getBoolean(nextProps.hide) || !hide;
 	},
 	render: function() {
-		var data = this.props.modal || {};
 		
 		return (
 			<div className={'fill orient-vertical-box w-detail-content w-detail-composer' + (util.getBoolean(this.props.hide) ? ' hide' : '')}>
 				<div className="w-composer-url box">
-					<input type="text" maxLength="8192" placeholder="url" className="fill w-composer-input" />
-					<select className="form-control w-composer-method">
+					<input ref="url" type="text" maxLength="8192" placeholder="url" className="fill w-composer-input" />
+					<select ref="method" className="form-control w-composer-method">
 		          		<option value="GET">GET</option>
 		          		<option value="POST">POST</option>
 		          		<option value="PUT">PUT</option>
@@ -47,8 +60,8 @@ var Composer = React.createClass({
 					<button className="btn btn-primary w-composer-execute">Execute</button>
 				</div>
 				<Divider vertical="true">
-					<textarea className="fill w-composer-headers" placeholder="headers"></textarea>
-					<textarea className="fill w-composer-body" placeholder="body"></textarea>
+					<textarea ref="headers" className="fill w-composer-headers" placeholder="headers"></textarea>
+					<textarea ref="body" className="fill w-composer-body" placeholder="body"></textarea>
 				</Divider>
 			</div>
 		);
