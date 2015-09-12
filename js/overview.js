aae70e11af3861697674be9feefe9c5ce8ecfcb4
avwo <avwu@qq.com>
2015-09-12 23:52:14 +0800

    add method

--- js/overview.js
+++ js/overview.js
@@ -11,9 +11,9 @@ var OVERVIEW_PROPS = ['url', 'realUrl', 'req.method', 'req.httpVersion', 'res.st
  * [req, res]delay://, method://, [req, res][content]Type://自动lookup, 
  * cache://xxxs[no], params://json|string(放在url)
  */
-var RULES = ['host', 'req', 'rule', 'res', 'weinre', 'filter', 'log', 'params', 'statusCode', 'redirect', 'delayReq', 
-             'reqSpeed', 'reqHeaders', 'method', 'reqType', 'reqBody', 'prependReq', 'appendReq', 'delayRes', 'resSpeed', 
-             'resHeaders', 'resType', 'cache', 'resBody', 'prependRes', 'appendRes'];
+var RULES = ['host', 'req', 'rule', 'res', 'weinre', 'filter', 'log', 'params', 'statusCode', 'redirect', 'method', 'cache', 
+             'delayReq', 'reqSpeed', 'reqHeaders', 'reqType', 'reqBody', 'prependReq', 'appendReq', 'delayRes', 'resSpeed', 
+             'resHeaders', 'resType', 'resBody', 'prependRes', 'appendRes'];
 var DEFAULT_OVERVIEW_MODAL = {};
 var DEFAULT_RULES_MODAL = {};
 
