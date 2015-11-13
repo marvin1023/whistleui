14f6ee52490193c4f8e31aeec124cf8bb4779ab8
avwo <avwu@qq.com>
2015-11-14 00:13:56 +0800

    add mode

--- js/rules-mode.js
+++ js/rules-mode.js
@@ -17,11 +17,11 @@ CodeMirror.defineMode('rules', function() {
 			}
 			
 			function isReq(str) {
-				return /^(?:req|referer|auth|ua|reqCookies|reqDelay|reqSpeed|reqCors|reqHeaders|method|reqType|reqBody|reqPrepend|reqAppend):\/\//.test(str);
+				return /^(?:req|referer|auth|ua|reqCookies|reqDelay|reqSpeed|reqCors|reqHeaders|method|reqType|reqBody|reqPrepend|reqAppend|reqReplace):\/\//.test(str);
 			}
 			
 			function isRes(str) {
-				return /^(?:res|resCookies|resHeaders|statusCode|redirect|resDelay|resSpeed|resCors|resType|cache|resBody|resPrepend|resAppend|css|html|js):\/\//.test(str);
+				return /^(?:res|resCookies|resHeaders|statusCode|redirect|resDelay|resSpeed|resCors|resType|cache|resBody|resPrepend|resAppend|css|html|js|resReplace):\/\//.test(str);
 			}
 			
 			function isUrl(str) {
