9f9e2478306fcf01e2e1021be6db3f5ae3745a3b
avwo <avwu@qq.com>
2016-05-07 15:58:49 +0800

    feat: show plugins options

--- js/menu-item.js
+++ js/menu-item.js
@@ -7,17 +7,23 @@ var MenuItem = React.createClass({
 	preventBlur: function(e) {
 		e.preventDefault();
 	},
+	stopPropagation: function(e) {
+		e.stopPropagation();
+	},
 	render: function() {
-		var options = this.props.options;
+		var self = this;
+		var options = self.props.options;
 		if (options && !options.length) {
 			options = null;
 		}
-		var name = this.props.name;
-		var onClick = this.props.onClick || util.noop;
-		var onClickOption = this.props.onClickOption || util.noop;
-		var onDoubleClickOption = this.props.onDoubleClickOption || util.noop;
+		var name = self.props.name;
+		var onClick = self.props.onClick || util.noop;
+		var onClickOption = self.props.onClickOption || util.noop;
+		var onDoubleClickOption = self.props.onDoubleClickOption || util.noop;
+		var checkedOptions = self.props.checkedOptions || {};
+		
 		return (
-			<div onBlur={this.props.onBlur} tabIndex="0" onMouseDown={this.preventBlur} style={{display: util.getBoolean(this.props.hide) ? 'none' : 'block'}} className={'w-menu-item ' + (this.props.className || '') + (this.props.disabled ? ' w-disabled' : '')}>
+			<div onBlur={self.props.onBlur} tabIndex="0" onMouseDown={self.preventBlur} style={{display: util.getBoolean(self.props.hide) ? 'none' : 'block'}} className={'w-menu-item ' + (self.props.className || '') + (self.props.disabled ? ' w-disabled' : '')}>
 			{
 					options ? <div className="w-menu-options">{options.map(function(option) {
 						
@@ -27,7 +33,8 @@ var MenuItem = React.createClass({
 								}}  onDoubleClick={function() {
 									onDoubleClickOption(option);
 								}} href="javascript:;">
-									<span className={'glyphicon glyphicon-' + (option.icon || 'asterisk')} style={{visibility: option.icon ? '' : 'hidden'}}></span>
+									{option.icon == 'checkbox' ? <input type="checkbox" data-name={option.name} onClick={self.stopPropagation} onChange={self.props.onChange} checked={!checkedOptions[option.name]} />
+											: <span className={'glyphicon glyphicon-' + (option.icon || 'asterisk')} style={{visibility: option.icon ? '' : 'hidden'}}></span>}
 									{option.name}
 								</a>
 						);
