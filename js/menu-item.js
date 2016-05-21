de46daceb2d1056597aa5870272e085e2c18b7ae
avwo <avwu@qq.com>
2016-05-21 21:41:26 +0800

    feat: disable all rules

--- js/menu-item.js
+++ js/menu-item.js
@@ -21,9 +21,10 @@ var MenuItem = React.createClass({
 		var onClickOption = self.props.onClickOption || util.noop;
 		var onDoubleClickOption = self.props.onDoubleClickOption || util.noop;
 		var checkedOptions = self.props.checkedOptions || {};
+		var disabled = self.props.disabled;
 		
 		return (
-			<div onBlur={self.props.onBlur} tabIndex="0" onMouseDown={self.preventBlur} style={{display: util.getBoolean(self.props.hide) ? 'none' : 'block'}} className={'w-menu-item ' + (self.props.className || '') + (self.props.disabled ? ' w-disabled' : '')}>
+			<div onBlur={self.props.onBlur} tabIndex="0" onMouseDown={self.preventBlur} style={{display: util.getBoolean(self.props.hide) ? 'none' : 'block'}} className={'w-menu-item ' + (self.props.className || '') + (disabled ? ' w-disabled' : '')}>
 			{
 					options ? <div className="w-menu-options">{options.map(function(option) {
 						
@@ -33,7 +34,7 @@ var MenuItem = React.createClass({
 								}}  onDoubleClick={function() {
 									onDoubleClickOption(option);
 								}} href="javascript:;">
-									{option.icon == 'checkbox' ? <input type="checkbox" data-name={option.name} onClick={self.stopPropagation} onChange={self.props.onChange} checked={!checkedOptions[option.name]} />
+									{option.icon == 'checkbox' ? <input type="checkbox" disabled={disabled} data-name={option.name} onClick={self.stopPropagation} onChange={self.props.onChange} checked={!checkedOptions[option.name]} />
 											: <span className={'glyphicon glyphicon-' + (option.icon || 'asterisk')} style={{visibility: option.icon ? '' : 'hidden'}}></span>}
 									{option.name}
 								</a>
