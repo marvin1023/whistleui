77189853eb1175464b39ccffe76bdb18125aac75
avwo <avwu@qq.com>
2016-05-27 21:31:32 +0800

    feat: Delay display options

--- js/index.js
+++ js/index.js
@@ -1212,7 +1212,7 @@ var Index = React.createClass({
 			self.setState({
 				hoverMenuList: true
 			});
-		}, 3000);
+		}, 1500);
 	},
 	leaveMenuList: function(e) {
 		clearTimeout(this._hoverMenuListTimer);
