d2ea8e2bbaaed754d2b6e8562ebfa0b02b50876c
avwo <avwu@qq.com>
2016-06-28 00:44:09 +0800

    feat: use delete & backspace to remove selected items

--- js/network.js
+++ js/network.js
@@ -1,4 +1,5 @@
 require('./base-css.js');
+var $ = require('jquery');
 var React = require('react');
 var util = require('./util');
 var events = require('./events');
@@ -8,6 +9,21 @@ var Detail = require('./detail');
 var dataCenter = require('./data-center');
 
 var Network = React.createClass({
+	componentDidMount: function() {
+		var self = this;
+		$(window).on('keydown', function(e) {
+			if (self.props.hide || e.target.nodeName == 'INPUT') {
+				return;
+			}
+			if (e.keyCode == 8 || e.keyCode == 46) {
+				var modal = self.props.modal;
+				if (modal && modal.removeSelectedItems()) {
+					self.setState({});
+				}
+				e.preventDefault();
+			}
+		});
+	},
 	shouldComponentUpdate: function(nextProps) {
 		var hide = util.getBoolean(this.props.hide);
 		return hide != util.getBoolean(nextProps.hide) || !hide;
