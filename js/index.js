8889136a8375269c109f6b4eae280dcf6d85deb9
avwo <avwu@qq.com>
2016-10-16 01:28:39 +0800

    style: Refine ui

--- js/index.js
+++ js/index.js
@@ -450,8 +450,6 @@ var Index = React.createClass({
 		if (this.state.name != 'plugins') {
 		  this.setMenuOptionsState();
 		  this.hidePluginsOptions();
-		} else {
-		  this.showPluginsOptions();
 		}
 		this.setState({
 			hasPlugins: true,
@@ -461,7 +459,6 @@ var Index = React.createClass({
 	},
 	showNetwork: function() {
 		if (this.state.name == 'network') {
-		  this.showNetworkOptions();
 			return;
 		}
 		this.setMenuOptionsState();
@@ -500,9 +497,7 @@ var Index = React.createClass({
 		if (this.state.name != 'rules') {
 		  this.setMenuOptionsState();
 		  this.hideRulesOptions();
-		} else {
-      this.showRulesOptions();
-    }
+		}
 		this.setState({
 			hasRules: true,
 			name: 'rules'
@@ -536,9 +531,7 @@ var Index = React.createClass({
 		if (this.state.name != 'values') {
 		  this.setMenuOptionsState();
 		  this.hideValuesOptions();
-		} else {
-      this.showValuesOptions();
-    }
+		}
 		this.setState({
 			hasValues: true,
 			name: 'values'
