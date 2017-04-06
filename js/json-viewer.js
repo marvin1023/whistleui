7daac31a8a1efc5823bea7d2b4d00286ed9a9e97
avwo <avwu@qq.com>
2017-04-06 23:14:27 +0800

    feat: Add react-json-tree

--- js/json-viewer.js
+++ js/json-viewer.js
@@ -24,8 +24,8 @@ var JsonViewer = React.createClass({
 		return (
 				<div className={'fill orient-vertical-box w-properties-wrap w-json-viewer' + (props.hide ? ' hide' : '')}>
 					<a onClick={this.toggle} className="w-properties-btn">{ viewSource ? 'view parsed' : 'view source' }</a>
-          <pre className={'w-json-viewer-str' + (viewSource ? '' : ' hide')}>{state.jsonStr}</pre>
-          <div className={'w-json-viewer-tree' + (viewSource ? ' hide' : '')}>
+          <pre className={'fill w-json-viewer-str' + (viewSource ? '' : ' hide')}>{state.jsonStr}</pre>
+          <div className={'fill w-json-viewer-tree' + (viewSource ? ' hide' : '')}>
             <JSONTree data={data} />
           </div>
 				</div>
