f491506796a18d61a9eb4e528fb1a3bc7bad9744
avwo <avwu@qq.com>
2017-04-12 03:40:42 +0800

    feat: Add textview tips

--- js/textarea.js
+++ js/textarea.js
@@ -13,7 +13,7 @@ var Tips = React.createClass({
 				<div className={'w-textview-tips' + (data.hide ? ' hide' : '')}>
 					<p>WebSocket</p>
 					<a href="https://github.com/whistle-plugins/whistle.websocket" target="_blank">
-						Click here for more information
+						Click here to install the plugin
 					</a>
 				</div>
 			);
