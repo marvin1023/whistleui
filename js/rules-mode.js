52f1357dbaf40c16105116fe8389f0178ba2a99b
avwo <avwu@qq.com>
2017-07-08 01:13:14 +0800

    feat: Add ~/path

--- js/rules-mode.js
+++ js/rules-mode.js
@@ -114,7 +114,7 @@ CodeMirror.defineMode('rules', function() {
 			}
 
 			function isWildcard(str) {
-				return /^(?:(?:https?:|wss?:|tunnel:)?\/\/)?\*\*?\./.test(str);
+				return /^(?:(?:https?:|wss?:|tunnel:)?\/\/)?(?:\$?\*\*?\.|\$?~\/)/.test(str);
 			}
 			
 			return {
