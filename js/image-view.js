2dcd1c47bf5df244648ff8ce40fbb210fa29457a
avwo <avwu@qq.com>
2017-08-28 00:21:04 +0800

    perf: Load as needed

--- js/image-view.js
+++ js/image-view.js
@@ -1,7 +1,12 @@
 require('../css/image-view.css');
 var React = require('react');
+var util = require('./util');
 
 var ImageView = React.createClass({
+  shouldComponentUpdate: function(nextProps) {
+		var hide = util.getBoolean(this.props.hide);
+		return hide != util.getBoolean(nextProps.hide) || !hide;
+	},
   render: function() {
     var props = this.props;
     return (
