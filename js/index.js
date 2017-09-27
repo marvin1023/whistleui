2ea46f4d1b5f426739430a69758658e3353d1f15
avwo <avwu@qq.com>
2017-09-27 17:26:59 +0800

    fix: Export file type by default

--- js/index.js
+++ js/index.js
@@ -331,7 +331,7 @@ var Index = React.createClass({
 			}
 		];
     protocols.setPlugins(state);
-		state.exportFileType = storage.get('exportFileType') || 'Fiddler';
+		state.exportFileType = storage.get('exportFileType');
 		state.showLeftMenu = storage.get('showLeftMenu');
 		return state;
 	},
